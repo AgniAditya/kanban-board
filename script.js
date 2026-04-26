17a608710cb872032cff8d32c12702420b9ac870
AgniAditya <agniaditya05@gmail.com>
2026-04-26 20:12:11 +0530

    refactor task management and enhance drag-and-drop functionality

--- script.js
+++ script.js
@@ -5,9 +5,27 @@ const todoStage = document.querySelector("#todo-stage")
 const inprogessStage = document.querySelector("#inprogress-stage")
 const completedStage = document.querySelector("#completed-stage")
 
-let todoTasks = [{name: "task1"},{name: "task4"},{name: "task6"}];
-let inProgessTasks = [{name: "task3"},{name: "task5"}];
-let completedTasks = [{name: "task2"}];
+let todoTasks = JSON.parse(localStorage.getItem("to do")) || [];
+let inProgessTasks = JSON.parse(localStorage.getItem("in progress")) || [];
+let completedTasks = JSON.parse(localStorage.getItem("done")) || [];
+
+let stages = [
+    {
+        stage: todoStage,
+        tasks: todoTasks
+    },
+    {
+        stage: inprogessStage,
+        tasks: inProgessTasks
+    },
+    {
+        stage: completedStage,
+        tasks: completedTasks
+    }
+];
+
+let draggedElement = null;
+let parentColumn = null;
 
 newTaskBtn.addEventListener("click", () => {
     modal.style.display = "flex";
@@ -33,9 +51,15 @@ form.addEventListener("submit", (e) => {
 function renderTasks(tasks,stage) {
     const stageTasks = stage.children[1];
     stageTasks.innerHTML = "";
-    tasks.forEach((target) => {
+    saveToLocalStorage(tasks,stage);
+    tasks.forEach((target,index) => {
         const task = document.createElement("div");
         task.classList.add("task");
+        task.setAttribute("draggable","true");
+        task.addEventListener("drag", (e) => {
+            draggedElement = task;
+            parentColumn = stage;
+        })
 
         const details = document.createElement("div");
         details.classList.add("details");
@@ -73,14 +97,54 @@ function renderTasks(tasks,stage) {
 }
 
 function init(){
-    renderTasks(todoTasks,todoStage);
-    renderTasks(inProgessTasks,inprogessStage);
-    renderTasks(completedTasks,completedStage);
+    stages.forEach((stage) => {
+        renderTasks(stage.tasks,stage.stage);
+        addDragAndDropEventsOnColumns(stage.stage)
+    })
 }
 
 function updateTotalTasks(tasks,stage) {
     const stagetotaltasks = stage.children[0].children[1].children[0];
     stagetotaltasks.textContent = tasks.length;
+}
+
+function saveToLocalStorage(tasks,stage){
+    const stageName = stage.children[0].children[0].textContent.toLowerCase();
+    localStorage.setItem(stageName,JSON.stringify(tasks))
+}
+
+function addDragAndDropEventsOnColumns(column){
+    column.addEventListener("dragenter",(e) => {
+        e.preventDefault()
+        column.classList.add('hover-over')
+    })
+    column.addEventListener("dragleave",(e) => {
+        e.preventDefault()
+        column.classList.remove('hover-over')
+    })
+    column.addEventListener("dragover",(e) => {
+        e.preventDefault();
+    })
+    column.addEventListener("drop",(e) => {
+        e.preventDefault();
+        column.classList.remove('hover-over')
+        const currentObj = stages.find((stageItem) => stageItem.stage === column);
+        const parentObj = stages.find((stageItem) => stageItem.stage === parentColumn);
+
+        const taskDescription = draggedElement.children[0].children[1].textContent;
+        const taskName = draggedElement.children[0].children[0].textContent;
+
+        currentObj.tasks.push({
+            name: taskName,
+            details: taskDescription
+        })
+        const index = parentObj.tasks.indexOf(draggedElement);
+        parentObj.tasks.splice(index,1);
+        updateTotalTasks(parentObj.tasks,parentObj.stage,-1);
+
+        renderTasks(currentObj.tasks,currentObj.stage)
+        renderTasks(parentObj.tasks,parentObj.stage)
+    })
 }
 
 init();
